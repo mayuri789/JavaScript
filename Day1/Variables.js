470ec80f3a916abc24be80bfd2303b2eada48c4b
mayuri789 <50314927+mayuri789@users.noreply.github.com>
2020-07-09 20:11:54 +0530

    Add files via upload

---
+++ Day1/Variables.js
@@ -0,0 +1,11 @@
+var fullName = "Mayuri Mestry"
+var courseName = "JavaScript"
+var isLoggedIn = true
+var loggedCount =34;
+console.log("LoggedCount : "+loggedCount)
+var paymentMode;
+console.log(paymentMode)
+paymentMode = "credit card"
+
+
+
